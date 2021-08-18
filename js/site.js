8e561fda32d84e90e08a13a11c3389cf3bf746ee
David Blackwelder <github@davidblackwelder.com>
2021-08-18 10:22:32 -0400

    Add functionality with javascript

--- js/site.js
+++ js/site.js
@@ -1,4 +1,55 @@
-// Call Hello World
-function helloWorld() {
-	alert("hello world");
+// get the values from the Page
+// starts/controller function
+function getValues() {
+	// get values from the page
+	let startValue = document.getElementById("startValue").value;
+	let endValue = document.getElementById("endValue").value;
+
+	// validate the input
+	// parse into Integers
+	startValue = parseInt(startValue);
+	endValue = parseInt(endValue);
+
+	if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+		// call generateNumbers
+		let numbers = generateNumbers(startValue, endValue);
+		// call displayNumbers
+		displayNumbers(numbers);
+	} else {
+		alert("You must enter integers.");
+	}
+}
+
+// generate numbers from startValue to the endValue
+// logic function(s)
+function generateNumbers(startValue, endValue) {
+	let numbers = [];
+
+	// get all numbers from start to end
+	for (let index = startValue; index <= endValue; index++) {
+		// this will execute in a loop until index = endValue
+		numbers.push(index);
+	}
+
+	return numbers;
+}
+
+// display the numbers and mark the even numbers bold
+// display/view function
+function displayNumbers(numbers) {
+	let templateRows = "";
+	for (let index = 0; index < numbers.length; index++) {
+		let className = "";
+		let number = numbers[index];
+
+		if (number % 2 === 0) {
+			className = "even";
+		} else {
+			className = "odd";
+		}
+
+		templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+	}
+
+	document.getElementById("results").innerHTML = templateRows;
 }
